3746db0cd2b899147c7d9c1447bca35a5b554c5b
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-07-26 18:05:53 +0300

    implement message subscriptions

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,24 +1,23 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { addMessageMutation, messagesQuery } from './queries';
+import { useMutation, useQuery, useSubscription } from "@apollo/client";
+import { addMessageMutation, messageAddedSubscription, messagesQuery } from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(addMessageMutation);
 
   const addMessage = async (text) => {
-    const { data: { message } } = await mutate({
+    const {
+      data: { message },
+    } = await mutate({
       variables: { text },
-      update: (cache, { data }) => {
-        console.log('[useAddmessage] data', data);
-        const newMessage = data.message;
-        cache.updateQuery({ query: messagesQuery}, ({ messages }) => {
-          return {
-            messages: [
-              ...messages,
-              newMessage
-            ]
-          }
-        })
-      }
+      // update: (cache, { data }) => {
+      //   console.log("[useAddmessage] data", data);
+      //   const newMessage = data.message;
+      //   cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+      //     return {
+      //       messages: [...messages, newMessage],
+      //     };
+      //   });
+      // },
     });
     return message;
   };
@@ -28,6 +27,17 @@ export function useAddMessage() {
 
 export function useMessages() {
   const { data } = useQuery(messagesQuery);
+  useSubscription(messageAddedSubscription, {
+    onData: ({ client, data }) => {
+      const newMessage = data.data.message;
+
+      client.cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+        return {
+          messages: [...messages, newMessage],
+        };
+      });
+    }
+  });
   return {
     messages: data?.messages ?? [],
   };
